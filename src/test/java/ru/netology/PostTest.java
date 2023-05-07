af53b7a1347ac38414741597dd46035556e1a255
Papillon <anil_061293@gmail.com>
2023-05-07 15:57:35 +0300

    Condig commit

---
+++ src/test/java/ru/netology/PostTest.java
@@ -0,0 +1,19 @@
+package ru.netology;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+    @Test
+    void shouldReturnCorrectRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("[0.body", equalTo("some data"));
+    }
+}
